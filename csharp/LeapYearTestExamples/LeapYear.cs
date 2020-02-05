f0950f140cd06ce96274732535357c3b9eca8304
Emily Bache <emily.bache@praqma.com>
2020-02-05 19:29:49 +0100

    added first csharp tests

---
+++ csharp/LeapYearTestExamples/LeapYear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LeapYearTestExamples
+{
+    public static class LeapYear
+    {
+        public static bool IsLeap(int year)
+        {
+            if (year % 400 == 0)
+                return true;
+            if (year % 100 == 0)
+                return false;
+            return year % 4 == 0;
+        }
+    }
+}
---
+++ csharp/LeapYearTestExamples/LeapYearXUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeapYearTestExamples
+{
+    public class LeapYearXUnitTest
+    {
+        [Fact]
+        public void CommonYear()
+        {
+            Assert.False(LeapYear.IsLeap(1993));
+        }
+        
+        [Fact]
+        public void TypicalLeap()
+        {
+            Assert.True(LeapYear.IsLeap(1992));
+        }
+        
+        [Fact]
+        public void AtypicalCommonYear()
+        {
+            Assert.False(LeapYear.IsLeap(1900));
+        }
+        
+        [Fact]
+        public void AtypicalLeap()
+        {
+            Assert.True(LeapYear.IsLeap(2000));
+        }
+    }
+}
